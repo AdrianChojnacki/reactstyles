24e121c0905fc39c6e414e11220f29433de88652
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-09-30 21:31:12 +0200

    lesson 3

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import "./App.css";
 import Text from "./Text";
 
 export default class App extends Component {
+  state = {
+    underline: false,
+  };
+
   render() {
+    const text = "Witaj na stronie";
+    // let classes = "";
+    // if (this.state.underline) classes += " line";
+    // if (text.length > 10) classes += " big";
+
+    const classes = ["one"];
+    if (this.state.underline) classes.push("line");
+    if (text.length > 10) classes.push("big");
+
     return (
-      <div className="app">
-        <h1>Witaj na stronie</h1>
+      <div className="app" onClick={() => this.setState({ underline: !this.state.underline })}>
+        <h1 className={classes.join(" ")}>{text}</h1>
         <Text />
       </div>
     );
