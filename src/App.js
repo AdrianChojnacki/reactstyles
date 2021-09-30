48b48e997995fdeb0f96a4ea9e5c3b9b55e8acae
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-09-30 21:23:52 +0200

    lesson 2

--- src/App.js
+++ src/App.js
@@ -1,60 +1,13 @@
 import React, { Component } from "react";
+import "./App.css";
+import Text from "./Text";
 
 export default class App extends Component {
-  state = {
-    active: false,
-  };
-
   render() {
-    // const styles = {
-    //   btn_off: {
-    //     display: "block",
-    //     width: 200,
-    //     margin: "20px auto",
-    //     padding: "10px 20px",
-    //     backgroundColor: "white",
-    //     border: "2px solid black",
-    //     fontFamily: "arial",
-    //     fontSize: 30,
-    //     cursor: "pointer",
-    //   },
-
-    //   btn_on: {
-    //     display: "block",
-    //     width: 200,
-    //     margin: "20px auto",
-    //     padding: "10px 20px",
-    //     backgroundColor: "cadetblue",
-    //     border: "2px solid black",
-    //     fontFamily: "arial",
-    //     color: "white",
-    //     fontSize: 30,
-    //     cursor: "pointer",
-    //   },
-    // };
-
-    let btn = {
-      display: "block",
-      width: 200,
-      margin: "20px auto",
-      padding: "10px 20px",
-      backgroundColor: "white",
-      border: "2px solid black",
-      fontFamily: "arial",
-      fontSize: 30,
-      cursor: "pointer",
-    };
-
-    if (this.state.active) {
-      btn.backgroundColor = "cadetblue";
-      btn.color = "white";
-    }
-
     return (
-      <div>
-        <button style={btn} onClick={() => this.setState({ active: !this.state.active })}>
-          {this.state.active ? "Wyłącz" : "Włącz"}
-        </button>
+      <div className="app">
+        <h1>Witaj na stronie</h1>
+        <Text />
       </div>
     );
   }
---
+++ src/Text.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Text.css";
+
+export default function Text() {
+  return (
+    <p className="red">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos id impedit, neque placeat
+      laudantium delectus, provident omnis repellat dolor voluptatem quis! Voluptas, cum praesentium
+      doloribus unde eius dignissimos! Pariatur, id!
+    </p>
+  );
+}
