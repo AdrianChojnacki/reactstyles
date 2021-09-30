ac7d84bd83fd275faad7f60c9dc21a72f90c381b
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-09-30 21:16:01 +0200

    first lesson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    active: false,
+  };
+
+  render() {
+    // const styles = {
+    //   btn_off: {
+    //     display: "block",
+    //     width: 200,
+    //     margin: "20px auto",
+    //     padding: "10px 20px",
+    //     backgroundColor: "white",
+    //     border: "2px solid black",
+    //     fontFamily: "arial",
+    //     fontSize: 30,
+    //     cursor: "pointer",
+    //   },
+
+    //   btn_on: {
+    //     display: "block",
+    //     width: 200,
+    //     margin: "20px auto",
+    //     padding: "10px 20px",
+    //     backgroundColor: "cadetblue",
+    //     border: "2px solid black",
+    //     fontFamily: "arial",
+    //     color: "white",
+    //     fontSize: 30,
+    //     cursor: "pointer",
+    //   },
+    // };
+
+    let btn = {
+      display: "block",
+      width: 200,
+      margin: "20px auto",
+      padding: "10px 20px",
+      backgroundColor: "white",
+      border: "2px solid black",
+      fontFamily: "arial",
+      fontSize: 30,
+      cursor: "pointer",
+    };
+
+    if (this.state.active) {
+      btn.backgroundColor = "cadetblue";
+      btn.color = "white";
+    }
+
+    return (
+      <div>
+        <button style={btn} onClick={() => this.setState({ active: !this.state.active })}>
+          {this.state.active ? "Wyłącz" : "Włącz"}
+        </button>
+      </div>
+    );
+  }
 }
-
-export default App;
