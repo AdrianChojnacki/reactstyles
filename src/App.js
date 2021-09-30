671a7e2205625cabb69520a93d10ec7a2b48b0dd
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-09-30 21:43:07 +0200

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Text from "./Text";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class App extends Component {
   state = {
@@ -18,10 +19,86 @@ export default class App extends Component {
     if (text.length > 10) classes.push("big");
 
     return (
-      <div className="app" onClick={() => this.setState({ underline: !this.state.underline })}>
-        <h1 className={classes.join(" ")}>{text}</h1>
-        <Text />
-      </div>
+      <>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+              Navbar
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    Link
+                  </a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Dropdown
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li>
+                      <a class="dropdown-item" href="#">
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <hr class="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled">Disabled</a>
+                </li>
+              </ul>
+              <form class="d-flex">
+                <input
+                  class="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button class="btn btn-outline-success" type="submit">
+                  Search
+                </button>
+              </form>
+            </div>
+          </div>
+        </nav>
+      </>
     );
   }
 }
--- src/Text.js
+++ src/Text.js
@@ -3,10 +3,13 @@ import "./Text.css";
 
 export default function Text() {
   return (
-    <p className="red">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos id impedit, neque placeat
-      laudantium delectus, provident omnis repellat dolor voluptatem quis! Voluptas, cum praesentium
-      doloribus unde eius dignissimos! Pariatur, id!
-    </p>
+    <>
+      <button className="btn btn-primary">BTN</button>
+      <p className="red">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos id impedit, neque placeat
+        laudantium delectus, provident omnis repellat dolor voluptatem quis! Voluptas, cum
+        praesentium doloribus unde eius dignissimos! Pariatur, id!
+      </p>
+    </>
   );
 }
